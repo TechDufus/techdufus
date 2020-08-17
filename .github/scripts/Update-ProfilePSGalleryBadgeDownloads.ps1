eaac821b5f49f6a74741d928ae02746e66bb9aa4
Matthew J. DeGarmo <46715299+matthewjdegarmo@users.noreply.github.com>
2020-08-17 12:19:40 -0500

    Updating infos

--- .github/scripts/Update-ProfilePSGalleryBadgeDownloads.ps1
+++ .github/scripts/Update-ProfilePSGalleryBadgeDownloads.ps1
@@ -45,8 +45,8 @@ if ($PSGalleryDownloads -gt $ProfileDownloads) {
   $OriginalREADME_CONTENT = Get-Content $Readme_path
   $NewREADME_CONTENT = $OriginalREADME_CONTENT -replace '-\d+-',"-$PSGalleryDownloads`-"
   Set-Content -Path $Readme_path -Value $NewREADME_CONTENT
-  git config --local user.email ${{ secrets.GH_EMAIL }}
-  git config --local user.name ${{ secrets.GH_USER }}
+  git config --local user.email 'matthewjdegarmo@gmail.com'
+  git config --local user.name 'Matthew J. DeGarmo'
   git commit -m "Updating PSGallery Downloads badge from $ProfileDownloads to $PSGalleryDownloads" -a
   git push
   exit 0
