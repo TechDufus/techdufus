1464258513a4d1deafe5601448f71c11b8e33a7f
Matthew J. DeGarmo <46715299+matthewjdegarmo@users.noreply.github.com>
2020-08-17 13:09:51 -0500

    Adding comma-seperator for larger numbers.

--- .github/scripts/Update-ProfilePSGalleryBadgeDownloads.ps1
+++ .github/scripts/Update-ProfilePSGalleryBadgeDownloads.ps1
@@ -41,9 +41,10 @@ Write-Output "Current Profile Downloads   : $ProfileDownloads"
 if ($PSGalleryDownloads -gt $ProfileDownloads) {
   ## NEED TO UPDATE LINK IN README
   Write-Output "Updating Git Profile README.ME badge."
+  $FormatedDownloadsNum = [String]::Format('{0:N0}',$PSGalleryDownloads)
   $Readme_path = [System.IO.Path]::Combine($GITHUB_WORKSPACE,'Readme.md')
   $OriginalREADME_CONTENT = Get-Content $Readme_path
-  $NewREADME_CONTENT = $OriginalREADME_CONTENT -replace '-\d+-',"-$PSGalleryDownloads`-"
+  $NewREADME_CONTENT = $OriginalREADME_CONTENT -replace '-\d+-',"-$FormattedDownloadsNum`-"
   Set-Content -Path $Readme_path -Value $NewREADME_CONTENT
   git config --local user.email 'matthewjdegarmo@gmail.com'
   git config --local user.name 'Matthew J. DeGarmo'
