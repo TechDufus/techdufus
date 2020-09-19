16e001d938d7e9800e3a979e2ad87195cbf88c20
Matthew DeGarmo <matthewjdegarmo@gmail.com>
2020-09-18 22:23:50 -0500

    Config change.

--- .github/scripts/Update-ProfilePSGalleryBadgeDownloads.ps1
+++ .github/scripts/Update-ProfilePSGalleryBadgeDownloads.ps1
@@ -1,7 +1,7 @@
 function Get-PSGalleryDownloads() {
     [CmdletBinding()]
     param (
-        [Parameter(Mandatory,ValueFromPipeline)]
+        [Parameter(Mandatory, ValueFromPipeline)]
         [System.String[]] $PackageName
     )
     begin {
@@ -17,13 +17,14 @@ function Get-PSGalleryDownloads() {
                 Write-Verbose "$_ downloads: $PackageDownloads"
                 $TotalDownloads = $TotalDownloads + $PackageDownloads
                 Write-Verbose "CurrentTotal: $TotalDownloads"
-                $ReturnObject += @{$_ = $PackageDownloads}
+                $ReturnObject += @{$_ = $PackageDownloads }
             }
-        } catch {
+        }
+        catch {
         }
     }
     end {
-        $ReturnObject += @{Total = $TotalDownloads}
+        $ReturnObject += @{Total = $TotalDownloads }
         [pscustomobject]$ReturnObject
     }
 }
@@ -32,24 +33,24 @@ function Get-CurrentProfileDownloadsValue() {
     ((Invoke-WebRequest "https://raw.githubusercontent.com/matthewjdegarmo/matthewjdegarmo/master/README.md").Content | Select-String -pattern "-\d+-").matches.value -replace '-'
 }
 
-$PSGalleryDownloads = (Get-PSGalleryDownloads -PackageName 'HelpDesk','AdminToolkit').Total
+$PSGalleryDownloads = (Get-PSGalleryDownloads -PackageName 'HelpDesk', 'AdminToolkit').Total
 $ProfileDownloads = Get-CurrentPRofileDownloadsValue
 Write-Output "Current PSGallery Downloads : $PSGalleryDownloads"
 Write-Output "Current Profile Downloads   : $ProfileDownloads"
 
 
 if ($PSGalleryDownloads -gt $ProfileDownloads) {
-  ## NEED TO UPDATE LINK IN README
-  Write-Output "Updating Git Profile README.ME badge."
-  $FormatedDownloadsNum = [String]::Format('{0:N0}',$PSGalleryDownloads)
-  $Readme_path = [System.IO.Path]::Combine($GITHUB_WORKSPACE,'Readme.md')
-  $OriginalREADME_CONTENT = Get-Content $Readme_path
-  $NewREADME_CONTENT = $OriginalREADME_CONTENT -replace '-\d+-',"-$FormatedDownloadsNum`-"
-  Set-Content -Path $Readme_path -Value $NewREADME_CONTENT
-  git config --local user.email 'matthewjdegarmo@gmail.com'
-  git config --local user.name 'Matthew J. DeGarmo'
-  git commit -m "Updating PSGallery Downloads badge from $ProfileDownloads to $PSGalleryDownloads" -a
-  try {
-    git push --quiet
-  } catch {}
+    ## NEED TO UPDATE LINK IN README
+    Write-Output "Updating Git Profile README.ME badge."
+    $FormatedDownloadsNum = [String]::Format('{0:N0}', $PSGalleryDownloads)
+    $Readme_path = [System.IO.Path]::Combine($GITHUB_WORKSPACE, 'Readme.md')
+    $OriginalREADME_CONTENT = Get-Content $Readme_path
+    $NewREADME_CONTENT = $OriginalREADME_CONTENT -replace '-\d+-', "-$FormatedDownloadsNum`-"
+    Set-Content -Path $Readme_path -Value $NewREADME_CONTENT
+    git config --local user.name 'Matthew J. DeGarmo'
+    git commit -m "Updating PSGallery Downloads badge from $ProfileDownloads to $PSGalleryDownloads" -a
+    try {
+        git push --quiet
+    }
+    catch {}
 }
